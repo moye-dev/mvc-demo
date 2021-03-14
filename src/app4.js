997f96f5359ba4dd7dba56ee1d218c9af41ea5ec
moye <2801845775@qq.com>
2021-03-14 23:04:25 +0800

    init

---
+++ src/app4.js
@@ -0,0 +1,9 @@
+import './app4.css'
+import $ from 'jquery'
+const $circle = $('#app4 .circle')
+$circle.on('mouseenter',()=>{
+    $circle.addClass('active')
+})
+$circle.on('mouseleave',()=>{
+    $circle.removeClass('active')
+})
